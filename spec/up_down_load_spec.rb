76b0fcaa10930c61e31b3151f0eef75d01a71861
Eric Laquer <laquereric@gmail.com>
2026-08-07 15:02:28 -0600

    license: strip stray Apache-2.0 SPDX headers -> LicenseRef-DataYoursSoftwareMine-1.0

--- spec/up_down_load_spec.rb
+++ spec/up_down_load_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # Copyright 2026 CBI BUSINESS TRANSACTIONS, LLC
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: LicenseRef-DataYoursSoftwareMine-1.0
 # Part of RailsRuntimes -- https://github.com/laquereric/DataYoursSoftwareMine
 
 require "spec_helper"
